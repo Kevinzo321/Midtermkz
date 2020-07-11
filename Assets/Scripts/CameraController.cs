945328016c0b6d27bfced6a90d8c04be3f8b0875
Kevin <kzhou10@my.centennialcollege.ca>
2020-07-10 20:22:44 -0400

    Update Jump Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Pri Insp War
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform MoveThreshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Camera Follow Player
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
